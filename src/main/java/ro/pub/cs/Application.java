159ba873ad13277d3756b097e09c0c8e0d548da7
bpaslaru <bgdn.paslaru@gmail.com>
2016-07-12 23:14:16 +0300

    Added project setup

---
+++ src/main/java/ro/pub/cs/Application.java
@@ -0,0 +1,15 @@
+package ro.pub.cs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Created by Bogdan on 7/10/2016.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/ro/pub/cs/controllers/GreetingController.java
@@ -0,0 +1,19 @@
+package ro.pub.cs.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by Bogdan on 7/10/2016.
+ */
+@Controller
+public class GreetingController {
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
